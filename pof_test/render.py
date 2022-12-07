3e16d982fbb499caf79745a7b4ebea464c29152c
RufusMeatBoot <71909100+RufusMeatBoot@users.noreply.github.com>
2022-12-07 13:28:00 -0800

    Update render.py

--- pof_test/render.py
+++ pof_test/render.py
@@ -1,12 +1,16 @@
 import game_map as Map
 import player as Player
+import objects as Obj
 import os
 
 def clearScreen():
     os.system("cls")
 
 def render():
+    if Map.gameMap[Player.player.pos] not in Obj.last:
+        Map.gameMap[Player.player.pos] = "@"
+    else:
+        Map.gameMap[Player.player.pos] = "@\n"
     clearScreen()
-    Map.gameMap[Player.player.pos] = "@"
     render = ''.join(str(item) for item in Map.gameMap)
     print(render)
